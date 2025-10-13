7c95a7e48e3edaa5d5550defa8100bf77199e2d7
dotx742 <xboy742100realnofake@gmail.com>
2025-10-13 18:32:38 -0400

    Now with changing time!
    With this fix, you can see the time variable change! I haven't added as much comments as with the Physicam...

--- physcene.gd
+++ physcene.gd
@@ -4,7 +4,21 @@ class_name Physcene
 
 @export_category("Time") ## Variables related to time and its passing.
 @export var time :float= 0 ## [i]t[/i] - the current relative scenic time.
-@export var time_bound :Vector2= Vector2(0,0) ## The maximum and minimum values for [member time]. Doesn't state the [member time_bounds_used].
-@export_flags("Lower","Upper") var time_bounds_used :int= 1 ## The [member time_bound]s being used.
+@export var time_bound :Vector2= Vector2(0,0) ## The minimum (x) and maximum (y) values for [member time]. Doesn't state the [member time_bounds_used].
+@export_enum("None:0", "Lower:1","Upper:2", "Both:3") var time_bounds_used :int= 1 ## The [member time_bound]s being used.
 @export var time_scale :float= 1 ## The scale at which [member time] changes. Pausing is separate of this.
-@export var paused :bool= true ## If [member time] is to stay constant. Your [member time_scale] is saved elsewhere.
+@export var paused :bool= false ## If [member time] is to stay constant. Your [member time_scale] is saved elsewhere.
+
+signal time_changed(del: float)
+
+func _process(delta: float) -> void:
+	if time_bounds_used % 4 == 3 and time_bound.x == time_bound.y:
+		time_scale = 0
+	if (time_bounds_used % 2 > 0.5) and (time < time_bound.x):
+		time = time_bound.x
+		time_scale *= -1
+	elif (time_bounds_used % 4 > 1) and (time > time_bound.y):
+		time = time_bound.y
+		time_scale *= -1
+	elif not paused: time += delta*time_scale
+	if time_scale != 0: emit_signal("time_changed", snappedf(time,0.01))
